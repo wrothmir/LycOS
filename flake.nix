d1ba2b403f8dbd6f9506845ef967eac5c279e1ba
nooodle-soup <vineetagarwal2402@gmail.com>
2024-09-13 12:25:42 -0700

    typo

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { nixpkgs, ... } @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
-        ./configuration.nix,
+        ./configuration.nix
       ];
     };
   };
