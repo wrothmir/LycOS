4f031c769acb12400de86aadb11dd28aff5da984
nooodle-soup <vineetagarwal2402@gmail.com>
2024-09-13 13:00:12 -0700

    Minor mods

--- flake.nix
+++ flake.nix
@@ -5,8 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { nixpkgs, ... } @ inputs: {
+  outputs = { nixpkgs, ... } @ inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
       ];
