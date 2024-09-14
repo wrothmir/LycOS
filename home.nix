99711afeaac6371b034dc268e02d962078e7a9b5
nooodle-soup <vineetagarwal2402@gmail.com>
2024-09-13 19:32:29 -0700

    Allow unfree in home

--- home.nix
+++ home.nix
@@ -36,6 +36,8 @@
     gcc
   ];
 
+  nixpkgs.config.allowUnfree = true;
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
