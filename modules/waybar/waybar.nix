e3d2c52163f5075ccf3b9b51e7c666a5e658372f
nooodle-soup <vineetagarwal2402@gmail.com>
2024-09-14 00:00:31 -0700

    waybar files -> config-waybar

--- modules/waybar/waybar.nix
+++ modules/waybar/waybar.nix
@@ -7,7 +7,7 @@
 
   home.file = {
     ".config/waybar/" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/pyros/modules/waybar";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/pyros/modules/waybar/config-waybar";
       recursive = true;
     };
   };
