b0ef4a45c43ca9181018db1baf3dc630af0bd685
nooodle-soup <vineetagarwal2402@gmail.com>
2024-09-14 04:44:23 -0700

    Testing rofi theme through programs.rofi

--- modules/rofi/rofi.nix
+++ modules/rofi/rofi.nix
@@ -3,12 +3,7 @@
 {
   programs.rofi = {
     enable = true;
+    theme = "gruvbox-light";
   };
 
-  home.file = {
-    ".config/rofi" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/PyrOS/modules/rofi/config-rofi";
-      recursive = true;
-    };
-  };
 }
