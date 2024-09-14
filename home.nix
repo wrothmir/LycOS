6ea51fddaed6eb89fb9e81d47be38e5d3ee639b5
nooodle-soup <vineetagarwal2402@gmail.com>
2024-09-13 19:55:16 -0700

    Testing fonts

--- home.nix
+++ home.nix
@@ -15,9 +15,11 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  fonts.fontconfig.enable = true;
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    (pkgs.nerdfonts.override { fonts = [ "Monoid" "DroidSansMono" ]; })
     alacritty
     zellij
     bat
