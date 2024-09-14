730d474cde56967fc1ebc41179d35f63e32705b5
nooodle-soup <vineetagarwal2402@gmail.com>
2024-09-14 03:29:04 -0700

    Configure Mako

--- modules/mako/mako.nix
+++ modules/mako/mako.nix
@@ -5,5 +5,9 @@
     enable = true;
     defaultTimeout = 4000;
     backgroundColor = "#fbf1c7";
+    textColor="#000000";
+    borderColor="#000000";
+    borderSize=3;
+    borderRadius=5;
   };
 }
