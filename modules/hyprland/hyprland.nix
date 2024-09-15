1742401c0d97181f97fa6622a9af515e262920b1
nooodle-soup <vineetagarwal2402@gmail.com>
2024-09-15 05:48:52 -0700

    Testing hypridle and hyprlock

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  services.hypridle = {
+    enable = true;
+  };
   home.file = {
     ".config/hypr" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/PyrOS/modules/hyprland/config-hyprland";
