fdfc51ab80c2043fbf516f846b9a592d15bb827b
raikan <vineetagarwal2402@gmail.com>
2024-10-12 02:25:42 -0700

    Testing nvim base

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -5,12 +5,41 @@
     enable = true;
     defaultEditor = true;
     vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-treesitter.withAllGrammars
+      lualine-nvim
+
+      telescope-nvim
+      telescope-file-browser-nvim
+
+      undotree
+
+      cmp-buffer
+      cmp-path
+      cmp-zsh
+      cmp-nvim-lua
+
+      luasnip
+      friendly-snippets
+      cmp_luasnip
+
+      nvim-lspconfig
+      cmp-nvim-lsp
+
+    ];
+    extraConfig = ''
+      :luafile ~/.config/nvim/lua/init.lua
+    '';
+
   };
 
-  home.file = {
-    ".config/nvim/" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/LycOS/modules/neovim/nvim";
-      recursive = true;
-    };
+  home.packages = with pkgs; [
+    stylua
+  ];
+
+  xdg.configFile.nvim = {
+    source = ./nvim;
+    recursive = true;
   };
+
 }
