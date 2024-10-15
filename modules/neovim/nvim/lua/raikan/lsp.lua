a74845bc18b3e7164e804defe7ea97a13698a294
raikan <vineetagarwal2402@gmail.com>
2024-10-15 16:43:58 -0700

    Add lsp capabilities

--- modules/neovim/nvim/lua/raikan/lsp.lua
+++ modules/neovim/nvim/lua/raikan/lsp.lua
@@ -1,7 +1,31 @@
+local capabilites = require('cmp_nvim_lsp').default_capabilities()
+
 local lspconfig = require("lspconfig")
 
-lspconfig.gopls.setup({})
+lspconfig.gopls.setup({
+  capabilites = capabilites,
+})
 lspconfig.lua_ls.setup({
+  capabilites = capabilites,
+  settings = {
+    Lua = {
+      runtime = {
+        path = {
+          '?.lua',
+          '?/init.lua',
+          '~/.config/nvim/pack/plugins/start/love2d/**/*.lua', -- Include the path to the Love2D addon
+        }
+      },
+      diagnostics = {
+        globals = { 'love' },
+      },
+      workspace = {
+        library = {
+          '~/.config/nvim/pack/plugins/start/love2d', -- Include the library path for the addon
+        }
+      }
+    }
+  },
   on_attach = function()
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
     vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
@@ -13,8 +37,8 @@ lspconfig.lua_ls.setup({
     vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
     vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { buffer = 0 })
     vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { buffer = 0 })
-    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { buffer = 0 })
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
+    vim.keymap.set("n", "<leader>vrf", vim.lsp.buf.references, { buffer = 0 })
+    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { buffer = 0 })
     vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = 0 })
   end
 })
