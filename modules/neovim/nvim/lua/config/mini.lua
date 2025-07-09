f4e70842c266137c557b939259ff0c3171326dd8
wrothmir <wrothmir@gmail.com>
2025-07-09 08:41:59 -0700

    Separate dotfiles

--- modules/neovim/nvim/lua/config/mini.lua
+++
@@ -1,9 +0,0 @@
-require 'mini.icons'.setup({})
-require 'mini.indentscope'.setup({
-  symbol = "│",
-  options = { try_as_border = true },
-  draw = {
-    delay = 0,
-    animation = function(s, n) return 0 end
-  },
-})
