065cb58b2e1f1f35ef03b4d9b663869fc58a4c24
wrothmir <wrothmir@gmail.com>
2025-08-17 20:23:56 -0700

    Messing with fonts

---
+++ fonts/fonts.nix
@@ -0,0 +1,38 @@
+# /fonts/fonts.nix
+{ pkgs }:
+
+let
+  fontDir = ./;  # relative path to folder containing TTFs
+
+  # list of fonts: use the exact filename of the TTF
+  fontNames = [
+    "Cuprum"
+    "OpenSans-Cond Light"
+    "OpenSans-Italic"
+    "PT-Sans-Narrow-Web-Regular"
+    "SegoeUI"
+    "SegUISB"
+    "Share-Regular"
+  ];
+
+  makeFont = fileName: pkgs.stdenv.mkDerivation {
+    pname = builtins.toLower fileName;
+    version = "1.0";
+    src = "${fontDir}/${fileName}.ttf";
+    installPhase = ''
+      mkdir -p $out/share/fonts/truetype
+      cp "$src" $out/share/fonts/truetype/
+    '';
+  };
+  localFonts = map makeFont fontNames;
+  nixFonts = [
+    pkgs.nerd-fonts.jetbrains-mono
+    pkgs.nerd-fonts.droid-sans-mono
+    pkgs.nerd-fonts.fira-code
+    pkgs.nerd-fonts.monoid
+    pkgs.ipafont
+    pkgs.kochi-substitute
+  ];
+
+in
+localFonts ++ nixFonts
